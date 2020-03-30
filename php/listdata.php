b47c2913a4df861978441202712a953648b9a737
timmyzzz <1738230225@qq.com>
2020-03-30 21:34:37 +0800

    0330

--- php/listdata.php
+++ php/listdata.php
@@ -1,16 +1,30 @@
 <?php
 include "conn.php";
 
+$pagesize = 12;//一页展示的数量
+
 $sql = "select * from xmgoods";
-
 $result = $conn->query($sql); //获取数据的结果集(记录集)
+$num = $result->num_rows; //记录集的总条数  28
+// 获取页数
 
+$pagenum = ceil($num / $pagesize); //获取页数  3
+if (isset($_GET['page'])) {
+    $pagevalue = $_GET['page'];
+} else {
+    $pagevalue = 1;
+}
+$page = ($pagevalue - 1) * $pagesize;
+
+
+$sql1 = "select * from xmgoods limit $page,$pagesize";
+$res = $conn->query($sql1);
 //通过二维数组输出
 // $result->num_rows; //记录集的条数
 // $result->fetch_assoc(); //逐条获取记录集的值，结果是数组。
 $arr = array();
-for ($i = 0; $i < $result->num_rows; $i++) {
-    $arr[$i] = $result->fetch_assoc();
+for ($i = 0; $i < $res->num_rows; $i++) {
+    $arr[$i] = $res->fetch_assoc();
 }
 
 echo json_encode($arr);//输出接口
--- src/script/list.js
+++ src/script/list.js
@@ -1,5 +1,9 @@
 !function($){
     const $list = $(".goods-list");
+    let array_default = [];//排序前的li数组
+    let array = [];//排序中的数组
+    let prev = null;
+    let next = null;
     $.ajax({
         url: 'http://localhost/object/php/listdata.php',
         dataType: 'json'
@@ -20,7 +24,85 @@
             </div>
             `;
         });
-        $strhtml+="</div>";
         $list.html($strhtml);
+        array_default = [];
+        array = [];
+        prev = null;
+        next = null;
+        $('.good-item').each(function (index, element) {
+            array[index] = $(this);
+            array_default[index] = $(this);
+        });
     });
+
+    $(".page").pagination({
+        pageCount: 3,//总页数
+        jump: true,//是否开启跳转到指定的页数，布尔值。
+        coping: true,//是否开启首页和尾页，布尔值。
+        prevContent: '上一页',
+        nextContent: '下一页',
+        homePage: '首页',
+        endPage: '尾页',
+        callback:function(d){
+           $.ajax({
+               url:"http://localhost/object/php/listdata.php",
+               data:{
+                   page: d.getCurrent()
+               },
+               dataType:"json"
+           }).done(function(data){
+            let $strhtml = "";
+            $.each(data,function(index, value){
+                $strhtml+=`
+                <div class='good-item'>
+                    <div class="good-img">
+                        <a href="detail.html?sid=${value.sid}">
+                            <img src="${value.url}" alt="">
+                        </a>
+                    </div>
+                            <h2>
+                                <a href='#'>${value.title}</a>
+                            </h2>
+                            <p class='price'>${value.price}</p>
+                </div>
+                `;
+            });
+            $list.html($strhtml);
+            array_default = [];
+            array = [];
+            prev = null;
+            next = null;
+            $('.good-item').each(function (index, element) {
+                array[index] = $(this);
+                array_default[index] = $(this);
+            });  
+           }) 
+        }
+    });
+
+    $(".faultbtn").on("click",function(){
+        $.each(array_default,function(index,value){
+            $(".goods-list").append(value);
+        })
+        return;
+    });
+    $(".pricebtn").on("click",function(){
+        for(let i = 0;i < array.length-1;i++){
+            for(let j=0;j<array.length-1-i;j++){
+                prev = parseFloat(array[j].find(".price").html())
+                next = parseFloat(array[j+1].find(".price").html())
+                if(prev>next){
+                    let middle = array[j];
+                     array[j] = array[j+1];
+                     array[j+1] = middle
+                }
+            }
+        }
+        $('.goods-list').empty();//清空原来的列表
+        $.each(array, function (index, value) {
+            $('.goods-list').append(value);
+        });
+    })
+
+
 }(jQuery);
